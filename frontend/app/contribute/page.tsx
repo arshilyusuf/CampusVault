4235866ae31cef737d8e43633d02b822e478167f
Arshil Yusuf <arshilyusuf@gmail.com>
2025-06-27 19:22:08 +0530

    remove export from consts

--- frontend/app/contribute/page.tsx
+++ frontend/app/contribute/page.tsx
@@ -17,9 +17,9 @@ const branches = [
   "Other",
 ];
 
-export const semesters = ["Select Semester","1", "2", "3", "4", "5", "6", "7", "8"];
+const semesters = ["Select Semester","1", "2", "3", "4", "5", "6", "7", "8"];
 
-export const uploadTypes = ["endsem", "midsem", "lectures", "notes", "other"];
+const uploadTypes = ["endsem", "midsem", "lectures", "notes", "other"];
 
 export default function ContributePage() {
   const { user } = useAuth();
